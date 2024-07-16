b05d65cf78effb1609d8c8e130c138984c582553
Sergey Koren <sergek@umd.edu>
2024-07-16 13:08:56 -0400

    fix formatting

--- hprc/ribotin.pm
+++ hprc/ribotin.pm
@@ -64,32 +64,32 @@ sub startRibotinAnalysis($$) {
     print CMD "module load seqtk\n";
     print CMD "\n";
     print CMD "\n";
-	print CMD "if [ ! -e ../assembly.homopolymer-compressed.gfa ]; then\n";
-	print CMD "   ln -s 5-untip/unitig-unrolled-unitig-unrolled-popped-unitig-normal-connected-tip.gfa ../assembly.homopolymer-compressed.gfa\n";
-	print CMD "fi\n";
-	print CMD "\n";
+    print CMD "if [ ! -e ../assembly.homopolymer-compressed.gfa ]; then\n";
+    print CMD "   ln -s 5-untip/unitig-unrolled-unitig-unrolled-popped-unitig-normal-connected-tip.gfa ../assembly.homopolymer-compressed.gfa\n";
+    print CMD "fi\n";
+    print CMD "\n";
     print CMD "if [ ! -e ribotin.out ] ; then\n";
-	print CMD "export PATH=\$PATH:/$root/software/ribotin/bin\n";
-	# we make a copy of the ribotin template sequences because liftoff was crashing when too many DBs were pointed to the same gff
-    print CMD "cp -r $root/software/ribotin/template_seqs ./\n"; 
+    print CMD "export PATH=\$PATH:/$root/software/ribotin/bin\n";
+    # we make a copy of the ribotin template sequences because liftoff was crashing when too many DBs were pointed to the same gff
+    print CMD "cp -r $root/software/ribotin/template_seqs ./\n";
     print CMD "ribotin-verkko --graphaligner $root/software/graphaligner/bin/GraphAligner --mbg $root/software/verkko/lib/verkko/bin/MBG \\\n";
     print CMD "    -x human \\\n";
-	print CMD "    --guess-tangles-using-reference ./template_seqs/chm13_rDNAs.fa \\\n";
-	print CMD "    --orient-by-reference           ./template_seqs/rDNA_one_unit.fasta \\\n";
-	print CMD "    --annotation-reference-fasta    ./template_seqs/rDNA_one_unit.fasta \\\n";
-	print CMD "    --annotation-gff3               ./template_seqs/rDNA_annotation.gff3 \\\n";
+    print CMD "    --guess-tangles-using-reference ./template_seqs/chm13_rDNAs.fa \\\n";
+    print CMD "    --orient-by-reference           ./template_seqs/rDNA_one_unit.fasta \\\n";
+    print CMD "    --annotation-reference-fasta    ./template_seqs/rDNA_one_unit.fasta \\\n";
+    print CMD "    --annotation-gff3               ./template_seqs/rDNA_annotation.gff3 \\\n";
     print CMD "    -i ../ \\\n";
     print CMD "    -o results \\\n";
     print CMD " > ribotin.WORKING \\\n";
     print CMD "&& \\\n";
-	print CMD "mv ribotin.WORKING ribotin.out \\\n";
+    print CMD "mv ribotin.WORKING ribotin.out \\\n";
     print CMD "|| \\\n";
     print CMD "mv ribotin.WORKING ribotin.FAIL\n";
-	print CMD "fi\n";
+    print CMD "fi\n";
     print CMD "\n";
     print CMD "rm -f ./ribotin.jid\n";
-	print CMD "rm -rf ./tmp\n";
-	print CMD "rm -rf ./template_seqs\n";
+    print CMD "rm -rf ./tmp\n";
+    print CMD "rm -rf ./template_seqs\n";
     print CMD "\n";
     print CMD "exit 0\n";
   }
